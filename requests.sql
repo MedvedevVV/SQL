2bce41e8fb8984a09f5daa33fd9a2c966e84c03e
MedvedevVV <107478820+MedvedevVV@users.noreply.github.com>
2022-08-25 13:53:19 +0300

    Update requests.sql

--- requests.sql
+++ requests.sql
@@ -4,7 +4,7 @@ SELECT * FROM `default` WHERE Type IN ('One', 'Two') ORDER BY Time_stamp DESC LI
 SELECT * FROM `default` WHERE BytesIn BETWEEN 2000 AND 3000 ORDER BY Time_stamp ASC LIMIT 100
 -- Все события таблицы default, для которых в поле BytesIn значение полученного трафика находится в диапазоне от 2000 до 3000 байт, отсортированные по столбцу Time_stamp в порядке возрастания. Количество отображаемых строк – 100.
 
-SELECT * FROM `default` WHERE Note LIKE '%ssipusers%' ORDER BY Time_stamp DESC LIMIT 100
+SELECT * FROM `default` WHERE Note LIKE '%sipusers%' ORDER BY Time_stamp DESC LIMIT 100
 -- Все события таблицы default, которые в поле Note содержат данные, соответствующие заданному шаблону %sipusers% в нижнем регистре, и отсортированы по столбцу Time_stamp в порядке убывания. Количество отображаемых строк – 100.
 
 SELECT * FROM `default` WHERE inSubnet(DeviceAddr, '192.168.0.0/16') ORDER BY Time_stamp DESC LIMIT 100
